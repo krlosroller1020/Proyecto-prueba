8f16b9a7db49aa15c65972e5f5187c847bab5cc2
krlosLopez <krlosroller@gmail.com>
2021-06-01 09:38:40 -0500

    first commit

---
+++ frontend/src/srceens/ProductScreen.js
@@ -0,0 +1,12 @@
+const Error404Screen = {
+  
+ 
+  
+  render: () => {
+    
+    
+    return `<div><body background="pulsera (25).jpeg"><a href="/images/pulsera (1).JPEG">Product Descriptcion</a></div>`;
+
+  },
+};
+export default Error404Screen;
